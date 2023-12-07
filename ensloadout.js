fa5e0358e81d65ac43995fbf7f018e3e00837a95
Kenneth Brook <kenneth.brook@hotmail.com>
2023-12-07 12:58:52 -0500

    matrix call added

--- ensloadout.js
+++ ensloadout.js
@@ -2,4 +2,21 @@ const rootDiv = document.getElementById("ENSLoadOut");
 
 const loading = document.createElement("h1");
 rootDiv.appendChild(loading);
-loading.innerText = `LOADING ${clientID}`
+loading.innerText = `LOADING ${clientID}`;
+
+async function portalOpen() {
+    try {
+        const response = await fetch(`http://ec2-3-143-219-120.us-east-2.compute.amazonaws.com/client/${clientID}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        console.log('Client Information:', data);
+        // Handle the retrieved data as needed in your application
+      } catch (error) {
+        console.error('Error fetching client information:', error.message);
+    }
+}
+
+portalOpen(clientID)
