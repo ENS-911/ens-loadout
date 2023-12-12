3c79f0c12e7f7de78e819d0ccc96270e5afd71d0
Kenneth Brook <kenneth.brook@hotmail.com>
2023-12-12 14:53:31 -0500

    parssing returned data

--- ensloadout.js
+++ ensloadout.js
@@ -4,6 +4,8 @@ const loading = document.createElement("h1");
 rootDiv.appendChild(loading);
 loading.innerText = `LOADING ${clientID}`;
 
+let found = ""
+
 async function portalOpen() {
     console.log(clientID)
     try {
@@ -15,7 +17,11 @@ async function portalOpen() {
   
         const data = await response.json();
         console.log('Client Information:', data);
+        found = data;
         // Handle the retrieved data as needed in your application
+        const loaded = document.createElement("h2");
+        rootDiv.appendChild(loaded);
+        loaded.innerText = `CLIENT FOUND, SUBSCRIPTION LEVEL = ${found}`;
       } catch (error) {
         console.error('Error fetching client information:', error.message);
     }
