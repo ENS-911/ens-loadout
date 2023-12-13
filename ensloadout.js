fe49590c9b9e1b016cd1d992f923b6eae4a0e745
Kenneth Brook <kenneth.brook@hotmail.com>
2023-12-13 11:01:06 -0500

    added selection for subscription package

--- ensloadout.js
+++ ensloadout.js
@@ -22,6 +22,34 @@ async function portalOpen() {
         const loaded = document.createElement("h2");
         rootDiv.appendChild(loaded);
         loaded.innerText = `CLIENT FOUND, SUBSCRIPTION LEVEL = ${found}`;
+
+        function loadExternalScript() {
+            // Create a script element
+            const script = document.createElement('script');
+          
+            // Set the source URL of the external JavaScript file
+            script.src = `https://ensloadout.911emergensee.com/ens-packages/${found}.js`;
+          
+            // Set any attributes if needed, such as async or defer
+            // script.async = true;
+          
+            // Append the script element to the document's head
+            document.head.appendChild(script);
+          
+            // You can optionally add an event listener to handle the script's onload or onerror events
+            script.onload = function () {
+              console.log('External script loaded successfully');
+              // Perform actions after the script is loaded
+            };
+          
+            script.onerror = function () {
+              console.error('Error loading external script');
+              // Handle error if the script fails to load
+            };
+          }
+          
+          // Call the function when needed
+          loadExternalScript();
       } catch (error) {
         console.error('Error fetching client information:', error.message);
     }
